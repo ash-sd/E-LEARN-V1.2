c2b5e114a63aca88d0ccc149ca04142f74fdf2f7
Achraf Samoudi <ash-sd@pm.me>
2022-02-01 10:04:38 +0100

    1-Feb_uploaded

---
+++ login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>E-Classe | Login</title>
+  </head>
+  <body style="background: linear-gradient(68deg, rgba(0,193,254,1) 16%, rgba(250,255,193,1) 100%); ">
+      <div class="vh-100 d-flex justify-content-center align-items-center">
+      <div class="mx-auto">
+        <div class="bg-white shadow p-5" style="border-radius:20px;">
+          <!-- IDEA: logo -->
+          <div class="d-flex">
+            <h2 style="color:#00c1fe" class="fw-bolder d-inline">|</h2>
+            <h2 class="m-1 mb-5" style="font-weight:600;">E-Classe</h2>
+          </div>
+          <div class="text-center mb-5 mx-5">
+            <h3 class="text-uppercase">Sign in</h3>
+            <p class="mx-2">Enter your credentias to access your account</p>
+          </div>
+          <!-- IDEA: form -->
+          <div class="">
+            <form class="" action="index.php" method="post">
+              <div class="mb-3">
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control p-2" placeholder="Enter your email">
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="text" class="form-control p-2" placeholder="Enter your password">
+              </div>
+              <div class="d-grid gap-2 my-4">
+                <a href="./index.php" class="d-grid gap-2 text-decoration-none"><button type="button" class="btn btn-primary text-uppercase" style="background-color:#00c1fe;" name="button">Sign in</button></a>
+              </div>
+              <p class="text-center">Forgot your Password? <a href="#">Reset Password</a></p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ php/navbar.php
@@ -0,0 +1,13 @@
+<div class="flex-column w-100">
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a href="#"><i class="text-dark mx-3 bi bi-arrow-left-circle"></i></a>
+      <div class="d-flex align-items-center justify-content-center">
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        </form>
+        <a href="#"><i class="text-dark mx-3 bi bi-bell"></i></a>
+      </div>
+
+    </div>
+  </nav>
